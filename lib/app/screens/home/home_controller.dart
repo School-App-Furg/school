a4d2f796b09e354857c1ff5e9836ce97e80fbdad
teste <matheuss.sls@gmail.com>
2021-10-24 15:32:19 -0300

    feat: Alterações para equiparação com a develop

--- lib/app/screens/home/home_controller.dart
+++ lib/app/screens/home/home_controller.dart
@@ -1,15 +1,6 @@
 import 'package:mobx/mobx.dart';
-import 'package:school/app/core/models/notas.dart';
 part 'home_controller.g.dart';
 
 class HomeController = _HomeControllerBase with _$HomeController;
 
-abstract class _HomeControllerBase with Store {
-  @observable
-  List<Aluno> notas = [];
-
-  @action
-  buscarAlunos() {
-    notas = [Aluno(name: 'joão', nota: 9), Aluno(name: 'maria', nota: 10)];
-  }
-}
+abstract class _HomeControllerBase with Store {}
--- lib/app/screens/home/home_controller.g.dart
+++ lib/app/screens/home/home_controller.g.dart
@@ -9,25 +9,10 @@ part of 'home_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$HomeController on _HomeControllerBase, Store {
-  final _$notasAtom = Atom(name: '_HomeControllerBase.notas');
-
-  @override
-  List<Aluno> get notas {
-    _$notasAtom.reportRead();
-    return super.notas;
-  }
-
-  @override
-  set notas(List<Aluno> value) {
-    _$notasAtom.reportWrite(value, super.notas, () {
-      super.notas = value;
-    });
-  }
-
   @override
   String toString() {
     return '''
-notas: ${notas}
+
     ''';
   }
 }

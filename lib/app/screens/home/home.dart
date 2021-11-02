29425aaf0b8f420e40cb91b2f25114377234c6fd
jardeldargas <jardeldargas@hotmail.com>
2021-11-02 18:34:32 -0300

    home screen design in progress

--- lib/app/screens/home/home.dart
+++ lib/app/screens/home/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:school/app/screens/home/home_controller.dart';
 
+import 'components/class_card.dart';
+
 class Home extends StatefulWidget {
   @override
   State<Home> createState() => _HomeState();
@@ -16,8 +18,73 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('home'),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.5,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.menu),
+          color: Colors.black,
+        ),
+        title: const Text(
+          'School App',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+            color: Colors.black,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+            color: Colors.black,
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: const [
+            ClassCard(
+              imagem:
+                  'https://www.gstatic.com/classroom/themes/img_breakfast.jpg',
+              disciplina: 'Biologia',
+              professor: 'Alice',
+            ),
+            ClassCard(
+              imagem:
+                  'https://www.gstatic.com/classroom/themes/img_bookclub.jpg',
+              disciplina: 'Inglês',
+              professor: 'Peter Parker',
+            ),
+            ClassCard(
+              imagem: 'https://www.gstatic.com/classroom/themes/Honors.jpg',
+              disciplina: 'História',
+              professor: 'Felipe',
+            ),
+            ClassCard(
+              imagem:
+                  'https://www.gstatic.com/classroom/themes/img_learnlanguage.jpg',
+              disciplina: 'Matemática',
+              professor: 'Karina',
+            ),
+            ClassCard(
+              imagem: 'https://www.gstatic.com/classroom/themes/img_code.jpg',
+              disciplina: 'Geografia',
+              professor: 'Leonel',
+            ),
+            ClassCard(
+              imagem: 'https://www.gstatic.com/classroom/themes/Chemistry.jpg',
+              disciplina: 'Química',
+              professor: 'Thor',
+            ),
+          ],
+        ),
       ),
     );
   }

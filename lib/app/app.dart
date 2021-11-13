0b3221a230b529d69af57c4a512b7e9152a98f53
Matheus Stallbaum Scheunemann <47986691+matheusscheu@users.noreply.github.com>
2021-11-12 21:56:39 -0300

    merge

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'screens/home/home_page.dart';
-import 'screens/splash/splash_screen.dart';
-=======
 import 'package:provider/provider.dart';
 import 'package:school/app/screens/repository/auth_repository.dart';
 
 import 'package:school/app/screens/auth/welcome/welcome.dart';
->>>>>>> develop
 import 'screens/auth/login/login_screen.dart';
 import 'screens/auth/splash/splash_screen.dart';
 import 'screens/home/home.dart';
@@ -40,13 +35,8 @@ class _RunState extends State<Run> {
         child: Splash(),
       ),
       routes: {
-<<<<<<< HEAD
-        '/home': (context) => HomePage(),
-        '/login': (context) => Login(),
-=======
         '/home': (context) => Home(),
         '/login': (context) => LoginScreen(),
->>>>>>> develop
         '/welcome': (context) => Welcome(),
         '/register': (context) => Register(),
         '/forgot': (context) => ForgotPasswordScreen(),
--- lib/app/screens/auth/splash.dart
+++ lib/app/screens/auth/splash.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import '../../core/navigation.dart';
+import 'package:school/app/core/service/navigation.dart';
 
 class Splash extends StatefulWidget {
   @override

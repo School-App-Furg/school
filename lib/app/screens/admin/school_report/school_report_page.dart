c3aeae63b002293f2c449015660549121bec06f0
jardeldargas <jardeldargas@hotmail.com>
2022-01-06 00:42:12 -0300

    Adição de TextButton nas telas de geração dos relatórios

--- lib/app/screens/admin/school_report/school_report_page.dart
+++ lib/app/screens/admin/school_report/school_report_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:school/app/core/components/app_bar_report.dart';
 
 import '../../../core/styles/colors.dart';
 import 'components/school_report_list.dart';
@@ -17,8 +19,12 @@ class _SchoolReportPageState extends State<SchoolReportPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Boletim'),
+      appBar: AppBarReport(
+        textButton: "Gerar Boletim Aluno",
+        onPressed: () {},
+        text: 'Boletim',
+        onPressedHistoric: () => Modular.to.pushNamed('./historic'),
+        onPressedSubject: () => Modular.to.pushNamed('./register-class'),
       ),
       body: SchoolReportList(),
       backgroundColor: lightGrey,
